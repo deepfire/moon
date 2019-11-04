7a21e44ba6ccfecf457150fcbb2d5624eb6868b2
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2019-11-04 23:40:13 +0300

    WIP cli:  embrace reflex-vty

---
+++ lift/MainReflexVty.hs
@@ -0,0 +1,210 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# OPTIONS_GHC -threaded #-}
+
+import Control.Applicative
+import Control.Monad
+import Control.Monad.Fix
+import Control.Monad.NodeId
+import Data.Functor.Misc
+import Data.Map (Map)
+import Data.Maybe
+import qualified Data.Map as Map
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Zipper as TZ
+import qualified Graphics.Vty as V
+import Reflex
+import Reflex.Network
+import Reflex.Class.Switchable
+import Reflex.Vty
+
+data Example = Example_TextEditor
+             | Example_Todo
+  deriving (Show, Read, Eq, Ord, Enum, Bounded)
+
+main :: IO ()
+main = mainWidget $ do
+  inp <- input
+  let buttons = col $ do
+        fixed 4 $ col $ do
+          fixed 1 $ text "Select an example."
+          fixed 1 $ text "Esc will bring you back here."
+          fixed 1 $ text "Ctrl+c to quit."
+        a <- fixed 5 $ textButtonStatic def "Todo List"
+        b <- fixed 5 $ textButtonStatic def "Text Editor"
+        return $ leftmost
+          [ Left Example_Todo <$ a
+          , Left Example_TextEditor <$ b
+          ]
+      escapable w = do
+        void w
+        i <- input
+        return $ fforMaybe i $ \case
+          V.EvKey V.KEsc [] -> Just $ Right ()
+          _ -> Nothing
+  rec out <- networkHold buttons $ ffor (switch (current out)) $ \case
+        Left Example_TextEditor -> escapable testBoxes
+        Left Example_Todo -> escapable taskList
+        Right () -> buttons
+  return $ fforMaybe inp $ \case
+    V.EvKey (V.KChar 'c') [V.MCtrl] -> Just ()
+    _ -> Nothing
+
+taskList
+  :: (Reflex t, MonadHold t m, MonadFix m, Adjustable t m, NotReady t m, PostBuild t m, MonadNodeId m)
+  => VtyWidget t m ()
+taskList = do
+  let btn = textButtonStatic def "Add another task"
+  inp <- input
+  let todos0 =
+        [ Todo "Find reflex-vty" True
+        , Todo "Become functional reactive" False
+        , Todo "Make vty apps" False
+        ]
+  rec let todos' = todos todos0 $ leftmost
+            [ () <$ e
+            , fforMaybe inp $ \case
+                V.EvKey V.KEnter [] -> Just ()
+                _ -> Nothing
+            ]
+      (m, (e, _)) <- splitV (pure (subtract 6)) (pure (True, True)) todos' $
+        splitV (pure (subtract 3)) (pure (True, True)) btn (display $ Map.size <$> current m)
+  return ()
+
+testBoxes
+  :: (Reflex t, MonadHold t m, MonadFix m, MonadNodeId m)
+  => VtyWidget t m ()
+testBoxes = do
+  dw <- displayWidth
+  dh <- displayHeight
+  let region1 = DynRegion (div' dw 6) (div' dh 6) (div' dw 2) (div' dh 2)
+      region2 = DynRegion (div' dw 4) (div' dh 4) (2 * div' dw 3) (2 * div' dh 3)
+  pane region1 (constDyn False) . boxStatic singleBoxStyle $ debugInput
+  _ <- pane region2 (constDyn True) . boxStatic singleBoxStyle $
+    let cfg = def
+          { _textInputConfig_initialValue =
+            "This box is a text input. The box below responds to mouse drag inputs. You can also drag the separator between the boxes to resize them."
+          }
+        textBox = boxStatic roundedBoxStyle $ multilineTextInput cfg
+        dragBox = boxStatic roundedBoxStyle dragTest
+    in splitVDrag (hRule doubleBoxStyle) textBox dragBox
+  return ()
+  where
+    div' :: (Integral a, Applicative f) => f a -> f a -> f a
+    div' = liftA2 div
+
+debugFocus :: (Reflex t, Monad m) => VtyWidget t m ()
+debugFocus = do
+  f <- focus
+  text $ T.pack . show <$> current f
+
+debugInput :: (Reflex t, MonadHold t m) => VtyWidget t m ()
+debugInput = do
+  lastEvent <- hold "No event yet" . fmap show =<< input
+  text $ T.pack <$> lastEvent
+
+dragTest :: (Reflex t, MonadHold t m, MonadFix m) => VtyWidget t m ()
+dragTest = do
+  lastEvent <- hold "No event yet" . fmap show =<< drag V.BLeft
+  text $ T.pack <$> lastEvent
+
+testStringBox :: (Reflex t, Monad m, MonadNodeId m) => VtyWidget t m ()
+testStringBox = boxStatic singleBoxStyle .
+  text . pure . T.pack . take 500 $ cycle ('\n' : ['a'..'z'])
+
+data Todo = Todo
+  { _todo_label :: Text
+  , _todo_done :: Bool
+  }
+  deriving (Show, Read, Eq, Ord)
+
+data TodoOutput t = TodoOutput
+  { _todoOutput_todo :: Dynamic t Todo
+  , _todoOutput_delete :: Event t ()
+  , _todoOutput_height :: Dynamic t Int
+  }
+
+instance Reflex t => Switchable t (TodoOutput t) where
+  switching t0 e = TodoOutput
+    <$> switching (_todoOutput_todo t0) (_todoOutput_todo <$> e)
+    <*> switching (_todoOutput_delete t0) (_todoOutput_delete <$> e)
+    <*> switching (_todoOutput_height t0) (_todoOutput_height <$> e)
+
+todo
+  :: (MonadHold t m, MonadFix m, Reflex t, MonadNodeId m)
+  => Todo
+  -> VtyWidget t m (TodoOutput t)
+todo t0 = do
+  w <- displayWidth
+  rec let checkboxWidth = 3
+          checkboxRegion = DynRegion 0 0 checkboxWidth 1
+          labelHeight = _textInput_lines ti
+          labelWidth = w - 1 - checkboxWidth
+          labelLeft = checkboxWidth + 1
+          labelTop = constDyn 0
+          labelRegion = DynRegion labelLeft labelTop labelWidth labelHeight
+      value <- pane checkboxRegion (pure True) $ checkbox def $ _todo_done t0
+      (ti, dd) <- pane labelRegion (pure True) $ do
+        i <- input
+        v <- textInput $ def { _textInputConfig_initialValue = TZ.fromText $ _todo_label t0 }
+        let deleteSelf = attachWithMaybe backspaceOnEmpty (current $ _textInput_value v) i
+        return (v, deleteSelf)
+  return $ TodoOutput
+    { _todoOutput_todo = Todo <$> _textInput_value ti <*> value
+    , _todoOutput_delete = dd
+    , _todoOutput_height = _textInput_lines ti
+    }
+  where
+    backspaceOnEmpty v = \case
+      V.EvKey V.KBS _ | T.null v -> Just ()
+      _ -> Nothing
+
+todos
+  :: forall t m.
+     ( MonadHold t m
+     , MonadFix m
+     , Reflex t
+     , Adjustable t m
+     , NotReady t m
+     , PostBuild t m
+     , MonadNodeId m
+     )
+  => [Todo]
+  -> Event t ()
+  -> VtyWidget t m (Dynamic t (Map Int (TodoOutput t)))
+todos todos0 newTodo = do
+  let todosMap0 = Map.fromList $ zip [0..] todos0
+  rec tabNav <- tabNavigation
+      let insertNav = 1 <$ insert
+          nav = leftmost [tabNav, insertNav]
+          tileCfg = def { _tileConfig_constraint = pure $ Constraint_Fixed 1}
+      listOut <- runLayout (pure Orientation_Column) 0 nav $
+        listHoldWithKey todosMap0 updates $ \k t -> tile tileCfg $ do
+          let sel = select selectOnDelete $ Const2 k
+          click <- void <$> mouseDown V.BLeft
+          pb <- getPostBuild
+          let focusMe = leftmost [ click, sel, pb ]
+          r <- todo t
+          return (focusMe, r)
+      let delete = ffor todoDelete $ \k -> Map.singleton k Nothing
+          updates = leftmost [insert, delete]
+          todoDelete = switch . current $
+            leftmost .  Map.elems . Map.mapWithKey (\k -> (k <$) . _todoOutput_delete) <$> listOut
+          todosMap = joinDynThroughMap $ fmap _todoOutput_todo <$> listOut
+          insert = ffor (tag (current todosMap) newTodo) $ \m -> case Map.lookupMax m of
+            Nothing -> Map.singleton 0 $ Just $ Todo "" False
+            Just (k, _) -> Map.singleton (k+1) $ Just $ Todo "" False
+          selectOnDelete = fanMap $ (`Map.singleton` ()) <$> attachWithMaybe
+            (\m k -> let (before, after) = Map.split k m
+                      in  fmap fst $ Map.lookupMax before <|> Map.lookupMin after)
+            (current todosMap)
+            todoDelete
+  return listOut
--- lift/src/CLI.hs
+++ lift/src/CLI.hs
@@ -1,18 +1,38 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
 module CLI (cli) where
 
 import           Codec.Serialise                          (Serialise)
+import           Control.Applicative
+import           Control.Concurrent
+import           Control.Monad
+import           Control.Monad.Fix
+import           Control.Monad.NodeId
 import           Control.Tracer
                  (Tracer, stdoutTracer, showTracing, traceWith)
-import qualified Data.ByteString.Lazy                   as LBS
+import qualified Control.Zipper                         as Zip
+import           Data.Functor.Misc
+import qualified Data.IntervalMap.FingerTree            as IMap
+import           Data.Map (Map)
+import           Data.Maybe
 import           Data.Maybe
 import           Data.Text
-import qualified Network.WebSockets                     as WS
-import           Options.Applicative
+import           Data.Text (Text)
+import           Options.Applicative               hiding (switch)
 import           Options.Applicative.Common
-
-import           Control.Concurrent
+import           Reflex                            hiding (Request)
+import           Reflex.Class.Switchable
+import           Reflex.Network
+import           Reflex.Vty                        hiding (Request)
+import qualified Data.ByteString.Lazy                   as LBS
+import qualified Data.Map                               as Map
+import qualified Data.Text                              as T
+import qualified Data.Text.Zipper                       as TZ
+import qualified Graphics.Vty                           as V
+import qualified Network.WebSockets                     as WS
 -- import qualified Control.Concurrent.STM.TMVar           as TM
 -- import qualified Control.Concurrent.STM.TBQueue         as TQ
+
+import           Text.Parsec.Pos (SourcePos)
 
 import           Control.Monad.Class.MonadSTM
 import           Control.Monad.Class.MonadThrow
@@ -23,9 +43,10 @@ import           Network.TypedProtocol.Core
 import qualified Network.TypedProtocol.Core             as Net
 import qualified Network.TypedProtocol.Driver           as Net
 
+import Basis hiding (Dynamic)
 import Ground.Hask
 import Lift
-import Pipe
+import Pipe hiding (Dynamic)
 import Wire.Peer
 import Wire.Protocol
 
@@ -42,12 +63,11 @@ cli = do
           chan   = channelFromWebsocket conn
       runClient tracer (client tracer req) $ channelFromWebsocket conn
 
-client
-  :: forall rej m a
-  . (rej ~ Text, m ~ IO, a ~ Reply)
-  => Tracer m String
-  -> Request
-  -> m (ClientState rej m a)
+client :: forall rej m a
+          . (rej ~ Text, m ~ IO, a ~ Reply)
+       => Tracer m String
+       -> Request
+       -> m (ClientState rej m a)
 client tracer req = pure $
   ClientRequesting req handleReply
  where
@@ -60,17 +80,241 @@ client tracer req = pure $
        withSomeValue TPoint (Proxy @(PipeSpace (SomePipe ()))) rep
        (\(VPoint space) -> do
            traceWith tracer $ "Got pipes.."
-           case (,) (lookupSpace "meta.fromrep" space)
-                    (lookupSpace "meta.unit" space)
-             of (,) (Just fromrep)
-                    (Just unit) -> do
-                  case apply (app opsDesc) fromrep
-                       (SomeValue
-                        (SomeKindValue
-                         TPoint (VPoint (Name "Unit" :: Name Type)))) of
-                    Left e -> putStrLn (unpack e)
-                    Right p -> putStrLn (show p)
+           interact' space
        ) of
        Right x -> x
        Left  e -> fail (unpack e)
      pure ClientDone
+
+type ReflexVty t m = (Reflex t, MonadHold t m, MonadFix m, Adjustable t m, NotReady t m, PostBuild t m, MonadNodeId m)
+
+data Hints = Hints
+  { unHints :: [SomeDesc]
+  }
+
+data HintsOutput t = HintsOutput
+  { hoHints   :: Dynamic t Hints
+  -- , hoCurrent ::
+  }
+
+interact'
+  :: PipeSpace (SomePipe ()) -> IO ()
+interact' s = mainWidget $ mdo
+  inp <- input
+  (,) w h <- (,) <$> displayWidth <*> displayHeight
+
+  descsD <- holdDyn descs never
+  descFilterD <- holdDyn (const True) $ updated filtD
+  descsFiltereD <- pure $ zipDynWith (\ds f-> Prelude.filter f ds) descsD descFilterD
+  msdD <- pane (menuR w) (pure True) $ col $ do
+    sdE <- fixed (const 15 . Prelude.length <$> descsFiltereD) $
+      selectSomeDesc (menuR w) descsFiltereD
+    -- selsD <- holdDyn "" $ showName . someDescName <$> traceEvent "sdE" sdE
+    -- fixedInert 1 $ text (("sel=" <>) <$> current selsD)
+    -- msdD <- holdDyn (listToMaybe descs) (Just <$> sdE)
+    msdD <- holdDyn Nothing (Just <$> sdE)
+    pure $ msdD
+  filtD <- pane (inputR w) (pure True) $ col $ mdo
+    let mexp = parseLocated <$> i
+        err = fromMaybe "" . eitherLeft  <$> mexp -- Dynamic Text
+        exp = eitherRight <$> mexp -- Dynamic (Expr (SourcePos, QName Pipe, SourcePos))
+    i <- fmap _textInput_value . fixed 1 . row $ do
+      fixedInert 2 $ richTextStatic (V.withForeColor V.defAttr V.blue) "> "
+      stretch $ textInput def
+    fixedInert 3 $ richTextStatic (V.withForeColor V.defAttr V.red) (current err)
+    pure $ (\i -> (\(someDescName -> n) -> T.isInfixOf i $ showName n)) <$> i
+
+  return $ fforMaybe inp $ \case
+    V.EvKey (V.KChar 'c') [V.MCtrl] -> Just ()
+    _ -> Nothing
+ where
+   ndescs = Prelude.length descs
+   menuR, inputR :: Reflex t => Dynamic t Int -> DynRegion t
+   menuR  w = DynRegion 0 0 w (pure $ 15)
+   inputR w = DynRegion 0 (pure 15) w 4
+   descs  = sortBy (compare `on` someDescName) $
+            somePipeDesc <$> psfrom s
+   psfrom = pipesFrom (SomeTypeRep $ typeRep @())
+   screen = DynRegion 0 0
+   escapable w = do
+     void w
+     i <- input
+     return $ fforMaybe i $ \case
+       V.EvKey V.KEsc [] -> Just $ Nothing
+       _ -> Nothing
+
+selectSomeDesc
+  :: forall t m a
+  . ( Adjustable t m, MonadFix m, MonadHold t m, PostBuild t m, MonadNodeId m, Reflex t
+    , a ~ ())
+  => DynRegion t
+  -> Dynamic t [SomeDesc] -- KEY -- this only triggers change if it is changed
+  -> VtyWidget t m (Event t a)
+selectSomeDesc r xsD = mdo
+  xs0 <- sample $ current xsD
+  -- networkHold buttons $ ffor (switch (current out))
+  -- networkHold (menu xs0) $ fmap menu (updated xsD)
+  rec out :: Dynamic t (Event t a)
+        <- networkHold (menu xs0) $ ffor (switch (pure $ updated xsD))
+           (menu
+            :: [SomeDesc] -> VtyWidget t m (Event t a))
+
+  pure $ switchDyn out
+ where
+   menu :: [SomeDesc]
+        -> VtyWidget t m (Event t a)
+   menu xs =
+     leftmost <$>
+     pane r (pure True)
+     (col $ do
+       fixedInert 1 $ text "woot" --debugFocus -- self-defeating, huh?
+       fixedInert 1 $ text "yay" --debugFocus -- self-defeating, huh?
+       -- why does this obscure the entries below??
+       traverse (fixed 3 . textButtonStatic def . pack . show)
+         xs)
+       -- traverse (fixed 1 . focusButton (someDescSelectable nameWi sigWi))
+       --   xs)
+    where nameWi = Prelude.maximum $ T.length . showName . someDescName <$> xs
+          sigWi  = Prelude.maximum $ T.length . showSig  . someDescSig  <$> xs
+
+someDescSelectable
+  :: (MonadFix m, MonadHold t m, PostBuild t m, MonadNodeId m, Reflex t)
+  => Int
+  -> Int
+  -> SomeDesc
+  -> Behavior t Bool
+  -> VtyWidget t m ()
+someDescSelectable nameWi sigWi (SomeDesc pd) focusB = row $ do
+  fixed 7 $ debugFocus
+  fixed (pure nameWi) $
+    richText (textFocusStyle (V.withForeColor V.defAttr V.green) focusB)
+      (pure . justifyRight nameWi ' ' . showName $ pdName pd)
+  fixed 4 $
+    richText (textFocusStyle V.defAttr focusB)
+      (pure " :: ")
+  fixed (pure sigWi) $
+    richText (textFocusStyle (V.withForeColor V.defAttr V.blue) focusB)
+      (pure . justifyLeft   sigWi ' ' . showSig $ pdSig pd)
+ where
+   textFocusStyle attr focusB =
+     (RichTextConfig $ selecting
+       (flip V.withBackColor $ V.rgbColor 1 1 1)
+       (pure attr)
+       focusB)
+
+richTextStatic = richText . RichTextConfig . pure
+
+focusButton
+  :: (Reflex t, Monad m, MonadNodeId m)
+  => (a -> Behavior t Bool -> VtyWidget t m ())
+  -> a
+  -> VtyWidget t m (Event t a)
+focusButton child a = do
+  f <- focus
+  child a (current f)
+  m <- mouseUp
+  k <- key V.KEnter
+  return $ leftmost [a <$ k, a <$ m]
+
+col'
+  :: (MonadFix m, MonadHold t m, PostBuild t m, MonadNodeId m)
+  => VtyWidget t m (Event t Int)
+  -> Layout t m a
+  -> VtyWidget t m a
+col' navE child = do
+  nav <- navE
+  runLayout (pure Orientation_Column) 0 nav child
+
+fixedInert
+  :: (Reflex t, Monad m, MonadNodeId m)
+  => Dynamic t Int
+  -> VtyWidget t m a
+  -> Layout t m a
+fixedInert sz = tile (TileConfig
+                      { _tileConfig_constraint = Constraint_Fixed <$> sz
+                      , _tileConfig_focusable  = pure False
+                      })
+                . clickable
+
+clickable
+  :: (Reflex t, Monad m)
+  => VtyWidget t m a
+  -> VtyWidget t m (Event t (), a)
+clickable child = do
+  click <- mouseDown V.BLeft
+  a <- child
+  return (() <$ click, a)
+
+upDownNavigation :: (Reflex t, Monad m) => VtyWidget t m (Event t Int)
+upDownNavigation = do
+  fwd <- fmap (const 1) <$> key V.KDown
+  back <- fmap (const (-1)) <$> key V.KUp
+  return $ leftmost [fwd, back]
+
+selecting :: Reflex t => (V.Attr -> V.Attr) -> Behavior t V.Attr -> Behavior t Bool -> Behavior t V.Attr
+selecting attrXform attrB selB = comp <$> attrB <*> selB
+  where
+    comp attr False = attr
+    comp attr True  = attrXform attr
+
+testBoxes
+  :: (Reflex t, MonadHold t m, MonadFix m, MonadNodeId m)
+  => Text -> VtyWidget t m ()
+testBoxes initial = do
+  dw <- displayWidth
+  dh <- displayHeight
+  let region1 = DynRegion (div' dw 6) (div' dh 6) (div' dw 2) (div' dh 2)
+      region2 = DynRegion (div' dw 4) (div' dh 4) (2 * div' dw 3) (2 * div' dh 3)
+  pane region1 (constDyn False) . boxStatic singleBoxStyle $ debugInput
+  _ <- pane region2 (constDyn True) . boxStatic singleBoxStyle $
+    let cfg = def
+          { _textInputConfig_initialValue = TZ.fromText initial }
+        textBox = boxStatic roundedBoxStyle $ multilineTextInput cfg
+        dragBox = boxStatic roundedBoxStyle dragTest
+    in splitVDrag (hRule doubleBoxStyle) textBox dragBox
+  return ()
+  where
+    div' :: (Integral a, Applicative f) => f a -> f a -> f a
+    div' = liftA2 div
+
+debugInput :: (Reflex t, MonadHold t m) => VtyWidget t m ()
+debugInput = do
+  lastEvent <- hold "No event yet" . fmap show =<< input
+  text $ T.pack <$> lastEvent
+debugFocus :: (Reflex t, Monad m) => VtyWidget t m ()
+debugFocus = do
+  f <- focus
+  text $ T.pack . show <$> current f
+dragTest :: (Reflex t, MonadHold t m, MonadFix m) => VtyWidget t m ()
+dragTest = do
+  lastEvent <- hold "No event yet" . fmap show =<< drag V.BLeft
+  text $ T.pack <$> lastEvent
+
+   -- look :: PipeSpace (SomePipe ()) -> QName Pipe -> Either Text (SomePipe ())
+   -- look space name = lookupSpace name space &
+   --   guard ("No such pipe: " <> showQName name)
+   -- loop :: Cons.InputT IO ()
+   -- loop = do
+   --   minput <- Cons.getInputLine "> "
+   --   case minput of
+   --     Nothing -> pure ()
+   --     Just "quit" -> pure ()
+   --     Just input -> do
+   --       Cons.outputStrLn $ "Input was: " ++ input
+   --       case parse (pack input) of
+   --         Left e -> Cons.outputStrLn $ "Parse:  " ++ unpack e
+   --         Right exp ->
+   --           case compil e opsDesc (pure . look s) exp of
+   --             Left e -> Cons.outputStrLn $ "Compile:  " ++ unpack e
+   --             Right pipe -> Cons.outputStrLn $ "OK:  " <> show pipe
+   --       loop
+   -- case (,) (lookupSpace "meta.fromrep" space)
+   --            (lookupSpace "meta.unit" space)
+   --     of (,) (Just fromrep)
+   --            (Just unit) -> do
+   --          case apply (app opsDesc) fromrep
+   --               (SomeValue
+   --                (SomeKindValue
+   --                 TPoint (VPoint (Name "Unit" :: Name Type)))) of
+   --            Left e -> putStrLn (unpack e)
+   --            Right p -> putStrLn (show p)
