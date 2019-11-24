ababeff5a9503b4040108bb9b105e80c8d9318dd
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2019-11-24 06:04:36 +0300

    Debug.{Reflex,TraceErr}:  tracing to stderr

---
+++ lift/src/Debug/Reflex.hs
@@ -0,0 +1,23 @@
+ module Debug.Reflex
+where
+
+import Debug.TraceErr (traceErr)
+import Reflex
+
+
+traceErrDynWith :: Reflex t => (a -> String) -> Dynamic t a -> Dynamic t a
+traceErrDynWith f d =
+  let e' = traceEventWith f $ updated d
+      getV0 = do
+        x <- sample $ current d
+        traceErr (f x) $ return x
+  in unsafeBuildDynamic getV0 e'
+
+traceErrEventWith :: Reflex t => (a -> String) -> Event t a -> Event t a
+traceErrEventWith f = push $ \x -> traceErr (f x) $ return $ Just x
+
+trdyn :: Reflex t => (a -> String) -> Dynamic t a -> Dynamic t a
+trdyn = traceErrDynWith
+
+trev :: Reflex t => (a -> String) -> Event t a -> Event t a
+trev = traceErrEventWith
---
+++ lift/src/Debug/TraceErr.hs
@@ -0,0 +1,33 @@
+module Debug.TraceErr
+  ( traceErr
+  , traceIOErr
+  )
+where
+
+import Data.List (partition)
+
+import Control.Monad (when)
+import Foreign.C.String as Foreign
+import System.IO as IO
+import System.IO.Unsafe as Unsafe
+
+
+traceErr :: String -> a -> a
+traceErr string expr = Unsafe.unsafePerformIO $ do
+    traceIOErr string
+    return expr
+
+foreign import ccall unsafe "HsBase.h debugBelch2"
+   debugBelch :: Foreign.CString -> Foreign.CString -> IO ()
+
+traceIOErr :: String -> IO ()
+traceIOErr msg = do
+    Foreign.withCString "%s\n" $ \cfmt -> do
+     -- NB: debugBelch can't deal with null bytes, so filter them
+     -- out so we don't accidentally truncate the message.  See Trac #9395
+     let (nulls, msg') = partition (=='\0') msg
+     Foreign.withCString msg' $ \cmsg ->
+      debugBelch cfmt cmsg
+     when (not (null nulls)) $
+       Foreign.withCString "WARNING: previous trace message had null bytes" $ \cmsg ->
+         debugBelch cfmt cmsg
